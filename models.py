059f5e46adb7e8371dbeebcdf39e721466073c26
rikerveja <37624420+rikerveja@users.noreply.github.com>
2024-12-18 16:04:05 +0800

    Update models.py

--- models.py
+++ models.py
@@ -5,10 +5,9 @@ from sqlalchemy.sql import func
 # 用户与服务器的关联表（多对多关系）
 user_server_association = db.Table(
     'user_server_association',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('server_id', db.Integer, db.ForeignKey('servers.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('server_id', db.Integer, db.ForeignKey('servers.id'), primary_key=True),
     db.UniqueConstraint('user_id', 'server_id'),
-    db.MetaData(),
     __table_args__ = {'extend_existing': True}  # 允许重新定义
 )
 
